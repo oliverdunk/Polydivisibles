1509098de5ed69179ab6b408d514e4652dc46f2d
Oliver Dunk <oliver@oliverdunk.com>
2016-03-31 14:00:46 +0100

    Use a character array rather than a string to store available characters, resulting in a significant speed improvement.

--- src/main/java/com/oliverdunk/polydivisble/Polydivisible.java
+++ src/main/java/com/oliverdunk/polydivisble/Polydivisible.java
@@ -1,27 +1,35 @@
 package com.oliverdunk.polydivisble;
 
+import org.apache.commons.lang.ArrayUtils;
+
 import java.math.BigInteger;
+import java.util.Arrays;
 
 public class Polydivisible {
 
     //This string contains all digits available, append more to check further bases.
-    private static String digitList = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static char[] digitList = {'1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
+                                        'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                                        'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+                                        'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                                        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                                        't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
     public static void main(String[] args){
         //TODO: Should base one be included? Solution is trivial.
-        for(int i = 1; i < digitList.length(); i++){
+        for(int i = 1; i < digitList.length; i++){
             findPolydivisbles(i);
         }
     }
 
     /**
-     * Finds all polydivibles within a base, with console output.
+     * Finds all polydivisibles within a base, with console output.
      * @param base The base which should be checked.
      */
     private static void findPolydivisbles(int base) {
         System.out.println("---------------- Base " + (base < 10 ? "0" : "") + base + " ----------------");
         long startTime = System.currentTimeMillis();
-        String availableDigits = digitList.substring(0, base - 1);
+        char[] availableDigits = Arrays.copyOfRange(digitList, 0, base - 1);
         permutation("", availableDigits, base);
         System.out.println("Completed in " + (System.currentTimeMillis() - startTime) + "ms.");
         System.out.println("-----------------------------------------");
@@ -34,16 +42,16 @@ public class Polydivisible {
      * @param availableDigits Digits left after the previous permutation.
      * @param base The base which should be checked.
      */
-    private static void permutation(String prefix, String availableDigits, int base) {
-        int n = availableDigits.length();
+    private static void permutation(String prefix, char[] availableDigits, int base) {
+        int n = availableDigits.length;
         if(n == 0) {
             if (checkPermutation(prefix, base)) System.out.println("Solution found: " + prefix);
         }
         else {
             if(!checkPermutation(prefix, base)) return;
             for (int i = 0; i < n; i++) {
-                char using = availableDigits.toCharArray()[i];
-                permutation(prefix + using, availableDigits.replace(using + "", ""), base);
+                char using = availableDigits[i];
+                permutation(prefix + using, ArrayUtils.removeElement(availableDigits, using), base);
             }
         }
     }
@@ -55,9 +63,10 @@ public class Polydivisible {
      * @return If the permutation is polydivisible.
      */
     private static boolean checkPermutation(String permutation, int base){
-        if(permutation.length() == 0) return true;
+        int length = permutation.length();
+        if(length == 0) return true;
         BigInteger inBaseTen = new BigInteger(permutation, base);
-        if (!inBaseTen.remainder(BigInteger.valueOf(permutation.length())).equals(BigInteger.ZERO)) return false;
+        if (!inBaseTen.remainder(BigInteger.valueOf(length)).equals(BigInteger.ZERO)) return false;
         return true;
     }
 
